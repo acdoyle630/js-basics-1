9299ca1ff6e9caf9445b1d4854ac1676e1f95c31
Adam <acdoyle630@gmail.com>
2017-03-13 16:24:50 -1000

    Bark

--- basics.js
+++ basics.js
@@ -34,4 +34,10 @@ greet('Adam');
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+ var dog = {
+ 	name: "Spot",
+ 	bark: function (bark) {
+ 		console.log ("Bark");
+ 	}
+ };
 
